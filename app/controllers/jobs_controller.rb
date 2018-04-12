369f7f7984854d8a766109b1daa0f6248f8612df
Marla Glasser <mbglasser@gmail.com>
2018-04-12 16:28:58 -0400

    Add delete functionality

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,6 +12,22 @@ class JobsController < ApplicationController
 		redirect_to jobs_path
 	end
 
+	def edit
+		@job = Job.find(params[:id])
+	end
+
+	def update
+		@job = Job.find(params[:id])
+		@job.update_attributes(job_params)
+		redirect_to jobs_path
+	end
+
+	def destroy
+		@job = Job.find(params[:id])
+		@job.destroy
+		redirect_to jobs_path
+	end
+
 
 	private
 
